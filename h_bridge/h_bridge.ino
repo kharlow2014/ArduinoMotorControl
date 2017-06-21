72a59dd89daeadf02f2bb6767f9368081ee6923e
Keaton Harlow <kharlow2014@users.noreply.github.com>
2017-06-21 14:03:59 -0500

    Update h_bridge.ino

--- h_bridge/h_bridge.ino
+++ h_bridge/h_bridge.ino
@@ -3,45 +3,36 @@
 // Test for h-bridge using a microcontroller
 // One button for forward, one button for reverse
 
+#include <MotorControl.h>
+
 // Defining the pins for forward and reverse output
 #define EXTEND       2
 #define RETRACT      3
 // Defining the pins for forward and reverse input
 #define EXT_BUTTON   10
 #define RET_BUTTON   11
-// Defining the pins to check that h-bridge voltage is low on reverse side
-#define EXT_CHECK    6
-#define RET_CHECK    5
 
-void setup() {
-  pinMode(EXTEND, OUTPUT);
-  pinMode(RETRACT, OUTPUT);
-  pinMode(EXT_BUTTON, INPUT);
-  pinMode(RET_BUTTON, OUTPUT);
-  pinMode(EXT_CHECK, INPUT);
-  pinMode(RET_CHECK, INPUT);
-}
+// Initialize the MotorControl object
+MotorControl motor(EXTEND, RETRACT);
 
+// int to keep track of the status of the two buttons
 int fbutton = 0;
 int rbutton = 0;
 
+void setup() {
+  pinMode(EXT_BUTTON, INPUT);
+  pinMode(RET_BUTTON, INPUT);
+}
+
 void loop() {
+  // Read the status of the two buttons
   fbutton = digitalRead(EXT_BUTTON);
   rbutton = digitalRead(RET_BUTTON);
-  if(fbutton == HIGH && rbutton == LOW) {
-    digitalWrite(RETRACT, LOW);
-    if(digitalRead(RET_CHECK) == LOW) {
-      digitalWrite(EXTEND, HIGH);
-    }
-  }
-  else if(rbutton == HIGH && fbutton == LOW) {
-    digitalWrite(EXTEND, LOW);
-    if(digitalRead(EXT_CHECK) == LOW) {
-      digitalWrite(RETRACT, HIGH);
-    }
-  }
-  else {
-    digitalWrite(EXTEND, LOW);
-    digitalWrite(RETRACT, LOW);
-  }
+  
+  // Execute the motion according to the status of the buttons.
+  // If one is LOW and the other is HIGH, it moves in the corresponding direction
+  // If both are LOW or both are HIGH, stop motion
+  if(fbutton == HIGH && rbutton == LOW) motor.actuate(true);
+  else if(rbutton == HIGH && fbutton == LOW) motor.actuate(false);
+  else motor.kill();
 }
